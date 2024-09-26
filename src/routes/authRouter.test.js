871d37ef252d24393bdc0cb134f0015dea69dc0e
William Keith Pendleton <wkp23@byu.edu>
2024-09-26 16:25:26 -0600

    update(authRouter.test.js) added register and login test

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,3 +1,19 @@
-test("hello world", () => {
-    expect("hello" + " world").toBe("hello world");
+const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
+let testUserAuthToken;
+
+beforeAll(async () => {
+    testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
+    const registerRes = await request(app).post("/api/auth").send(testUser);
+    testUserAuthToken = registerRes.body.token;
+});
+
+test("login", async () => {
+    const loginRes = await request(app).put("/api/auth").send(testUser);
+    expect(loginRes.status).toBe(200);
+    expect(loginRes.body.token).toMatch(
+        /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/
+    );
+
+    const { password, ...user } = { ...testUser, roles: [{ role: "diner" }] };
+    expect(loginRes.body.user).toMatchObject(user);
 });
