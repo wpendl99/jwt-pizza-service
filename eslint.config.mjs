9b5d6a265ee6ec0dbf6279813be85f49c4397a65
William Keith Pendleton <wkp23@byu.edu>
2024-10-01 15:28:08 -0600

    update(eslint.config, jest.config, ci.yml) added auto testing pipeline, and fail on lint and <80% coverage

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,5 @@ export default [
     { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
     { languageOptions: { globals: globals.node } },
     { languageOptions: { globals: globals.jest } },
-    { ignores: ["**/*.test.js"] },
     pluginJs.configs.recommended,
 ];
